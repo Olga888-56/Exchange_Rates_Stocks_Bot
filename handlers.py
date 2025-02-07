79ce65274dac392734482a8a962ed62d30289925
Olga888-56 <o.glazunova86@gmail.com>
2025-02-07 16:22:19 +0500

    прикрутила mongo

--- handlers.py
+++ handlers.py
@@ -1,9 +1,11 @@
+from db import db, get_or_create_user
 from glob import glob
 from random import choice
 from telegram.ext import RegexHandler
 from utils import get_smile, main_keyboard
 
 def greet_user(update, context):
+    user = get_or_create_user(db, update.effective_user, update.message.chat.id)
     print("Вызван /start")
     context.user_data['emoji'] = get_smile(context.user_data)
   
@@ -14,11 +16,13 @@ def greet_user(update, context):
     
 
 def user_coordinates(update,context):
+    user = get_or_create_user(db, update.effective_user, update.message.chat.id)
     coords = update.message.location
     print(coords)
     update.message.reply_text(f"Ваши координаты {coords}", reply_markup=main_keyboard())
 
 
 def currencies_handler(update, context):
+    user = get_or_create_user(db, update.effective_user, update.message.chat.id)
     currencies = ['USD', 'EUR', 'GBP', 'JPY', 'CNY']
     update.message.reply_text("Выберите валюту:", reply_markup=ReplyKeyboardMarkup([currencies], resize_keyboard=True))
